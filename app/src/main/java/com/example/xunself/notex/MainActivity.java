e47ad2edd23475f57df37586e77960c1a58eb20a
XunselF <XunselF@hotmail.com>
2018-01-13 22:39:25 +0800

    add Note item data

--- app/src/main/java/com/example/xunself/notex/MainActivity.java
+++ app/src/main/java/com/example/xunself/notex/MainActivity.java
@@ -5,15 +5,32 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private FloatingActionButton add_note_button;
     //添加事件按钮
+
+    private RecyclerView noteRecyclerView;
+
+    private NoteAdapter noteAdapter;
+
+    private List<Note> noteList;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +39,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         init();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getData();
+    }
+
+
     /**
      * 初始化控件
      */
     private void init(){
+        noteList = new ArrayList<>();
         add_note_button = (FloatingActionButton) findViewById(R.id.add_note_button);
+        noteRecyclerView = (RecyclerView) findViewById(R.id.note_recyclerview);
+        noteAdapter = new NoteAdapter();
+        noteRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        noteRecyclerView.setAdapter(noteAdapter);
         add_note_button.setOnClickListener(this);
+
+    }
+
+    /**
+     * 获取数据
+     */
+    private void getData(){
+        Note note;
+        for (int i = 0; i < 2; i++){
+            note = new Note("123","123",2018,1,13);
+            noteList.add(note);
+        }
+        note = new Note("插入","插入的数据",2018,1,13);
+        noteList.add(1,note);
+        noteAdapter.notifyDataSetChanged();
     }
 
 
@@ -41,6 +85,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             default:
                 break;
+        }
+    }
+
+    class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.ViewHolder>{
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.note_layout,parent,false);
+            ViewHolder holder = new ViewHolder(view);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            Note note = noteList.get(position);
+            holder.noteYear.setText(note.getYear() + "");
+            holder.noteTitle.setText(note.getTitle());
+            holder.noteContent.setText(note.getContent());
+            holder.noteTime.setText((note.getMonth() + 1) + "-" + note.getDay());
+        }
+
+        @Override
+        public int getItemCount() {
+            return noteList.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder{
+            TextView noteYear;
+            TextView noteTitle;
+            TextView noteTime;
+            TextView noteContent;
+            public ViewHolder(View itemView) {
+                super(itemView);
+                noteYear = (TextView) itemView.findViewById(R.id.note_year);
+                noteTitle = (TextView) itemView.findViewById(R.id.note_title);
+                noteTime = (TextView) itemView.findViewById(R.id.note_time);
+                noteContent = (TextView) itemView.findViewById(R.id.note_content);
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/xunself/notex/Note.java
@@ -0,0 +1,41 @@
+package com.example.xunself.notex;
+
+/**
+ * Created by XunselF on 2018/1/13.
+ */
+
+public class Note {
+    private String title;
+    private String content;
+    private int year;
+    private int month;
+    private int day;
+
+    public Note(String title,String content,int year,int month,int day){
+        this.title = title;
+        this.content = content;
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
