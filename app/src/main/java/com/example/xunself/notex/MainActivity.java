c953aef3b37502ff0068d5c29d4ae5a881621745
XunselF <XunselF@hotmail.com>
2018-01-13 18:23:07 +0800

    add main about add note button

--- app/src/main/java/com/example/xunself/notex/MainActivity.java
+++ app/src/main/java/com/example/xunself/notex/MainActivity.java
@@ -9,14 +9,31 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private FloatingActionButton add_note_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        init();
     }
 
+    /**
+     * 初始化控件
+     */
+    private void init(){
+        add_note_button = (FloatingActionButton) findViewById(R.id.add_note_button);
+        add_note_button.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.add_note_button:
+                break;
+            default:
+                break;
+        }
+    }
 }
