de90778d3d84a7d74d74b103eeaa375acd1cb9a0
XunselF <XunselF@hotmail.com>
2018-01-13 17:52:52 +0800

    add splash in page

---
+++ app/src/main/java/com/example/xunself/notex/SplashActivity.java
@@ -0,0 +1,47 @@
+package com.example.xunself.notex;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+/**
+ * author:XunselF (XunselF@hotmail.com)
+ * 2018/1/12
+ * splash
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+    //延迟时长
+    private Handler splashDelayHandler;
+    //延迟线程
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //进入全屏模式
+        setContentView(R.layout.activity_splash);
+        splashDelay();
+    }
+
+    /**
+     * 定时器用于启动主页面
+     */
+    private void splashDelay(){
+        splashDelayHandler = new Handler();
+        splashDelayHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+                //退出全屏模式
+
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_DISPLAY_LENGTH);
+    }
+}
